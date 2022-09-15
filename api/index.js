f64ad9c3e57dcadc202657a691c5d805bfc8089d
Mariam Jolade <adeyemijolade@gmail.com>
2022-09-15 14:18:32 +0100

    bug fix again

--- api/index.js
+++ api/index.js
@@ -7,19 +7,19 @@ const shortURL = require("../middlewares/shortURL.js");
 const app = express();
 
 
-app.use(express.static(path.resolve('../public')));
+app.use(express.static(path.resolve('/public')));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 //  app.use("/shortUrls");
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve("../pages/index.html"))
+    res.sendFile(path.resolve("/pages/index.html"))
  }) 
 
  app.post("/shortUrls", shortURL, async (req, res) => {
    let data;
    try {
-      data = await fs.readFile(path.resolve("../data.json"), "utf-8");
+      data = await fs.readFile(path.resolve("/data.json"), "utf-8");
       data = JSON.parse(data);
       
    } catch (error) {
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
 
     data.push(newUrl);
 
-    await fs.writeFile(path.resolve("../data.json"), JSON.stringify(data));
+    await fs.writeFile(path.resolve("/data.json"), JSON.stringify(data));
 
     console.log(req.body);
     res.status(200).json(newUrl);
@@ -57,7 +57,7 @@ app.get('/', (req, res) => {
  app.get("/:slug", async (req, res)=>{
    let smallUrl;
    try {
-      smallUrl = await fs.readFile(path.resolve("../data.json"), "utf-8");
+      smallUrl = await fs.readFile(path.resolve("/data.json"), "utf-8");
       smallUrl = JSON.parse(smallUrl)
    } catch (error) {
       smallUrl = [];
